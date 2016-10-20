d122145fc59c618fc4e7f9eb24a3b526ba784401
cshea892 <cshea892@students.bju.edu>
2016-10-20 10:21:57 -0400

    chaned highscore.h and highscore.cpp

--- highscore.cpp
+++ highscore.cpp
@@ -1,5 +1,67 @@
 #include "highscore.h"
 
-HighScore::HighScore()
-{
-}
+		
+
+	//read in the high scores numbers and users from a file into two vectors
+	//or exits if there is no file with the specified name
+	void HighScore::LoadScore () {
+			string name;
+			int score;
+			ifstream file;
+			file.open ("highscores.txt");
+		
+		if (file.is_open()) {
+			while (file >> name >> score) {
+				scores.push_back(score);
+				names.push_back(name);
+			}
+		} else {
+			cout << "The file couldn't be opened" << endl;
+		}
+		file.close();
+	}
+	
+	//saves the high score of the current user to the vectors using the
+	//specified <string> and <score>
+	void HighScore::NewHighScore(string name, int score) {
+		
+		for (int i = 0; i < 10; i++) {
+			if (scores.at(i) < score) {
+				scores.insert(scores.begin() + i, score);
+				names.insert(names.begin() + i, name);
+				names.pop_back();
+				scores.pop_back();
+				return;
+			}
+				
+		}
+		
+	}
+	
+	//writes the scores and names in the vector to the file
+	void HighScore::SaveScores() {
+		ofstream file;
+		file.open ("highscores.txt");
+			if (file.is_open()) {
+				for (int i = 0; i < 10; i++) {
+					file << names.at(i)  <<" " << scores.at(i) << endl;
+				}
+		} else {
+			cout << "The file couldn't be opened" << endl;
+		}
+		file.close();
+	}
+	
+
+	void HighScore::UnitTests() {
+			//unit tests
+			HighScore h;
+			h.LoadScore();
+			assert(h.getScore(0) == 1200);
+			assert(h.getName(0) == "Jill");
+			h.NewHighScore("Joe", 500);
+			assert(h.getScore(5) == 500);
+			assert(h.getName(5) == "Joe");
+			h.SaveScores();
+			
+	}
--- highscore.h
+++ highscore.h
@@ -1,10 +1,25 @@
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
 
-class HighScore
-{
-public:
-    HighScore();
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+using namespace std;
+
+class HighScore {
+private:
+    vector<int> scores;
+    vector <string> names;
+public: 
+	void LoadScore();
+	void NewHighScore(string name, int score);
+	void SaveScores();
+	int getScore(int i) { return scores.at(i); };
+	string getName(int i) {return names.at(i); };
+        void UnitTests();
 };
 
 #endif // HIGHSCORE_H
