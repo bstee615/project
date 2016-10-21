63799357379156349e5f27c295930314141b19a6
cshea892 <cshea892@students.bju.edu>
2016-10-21 18:28:54 -0400

    Added methods to World.cpp and world.h and added a constructor in highscore.h to prevent copying

--- world.cpp
+++ world.cpp
@@ -1,5 +1,70 @@
 #include "world.h"
 
-World::World()
-{
+
+World World::world;
+
+// returns object with <id>, or NULL if none
+WorldObject *World::getById(int id) {
+    for (size_t i = 0; i < objects.size(); ++i) {
+        Object *obj = objects.at(i);
+        if (obj->getId() == id) {
+            return obj;
+        }
+    }
+    return NULL;
+}
+
+//erases the object with the specified <id> pointer from the world vector, and returns the
+//pointer if it is found, or nullptr if not
+WorldObject *World::destroy(int id) {
+    Object *obj = NULL;
+
+    auto iter = objects.begin();
+    while (iter != objects.end() && (*iter)->getId() != id) {
+        ++iter;
+    }
+
+    if (iter != objects.end()) {
+        // found it
+        obj = *iter;
+        objects.erase(iter);
+    }
+    return obj;
+}
+
+//Resets the world to, both by deleting memory and erasing the pointers
+//to that memory
+void World::reset() {
+	
+    for (size_t i = 0; i < objects.size(); ++i) {
+        delete objects.at(i);
+    }
+    objects.clear();
+    Object::resetNextId();
+}
+
+//World destructor
+World::~World() {
+    reset();
+}
+
+// factory: creates and returns an Object or derived class, determined by
+// <type>. Returns NULL if <type> is not recognized.
+Object *World::createObject(const string& type) {
+    if (type.find("ob") == 0)
+        return new Object;
+    else if (type.find("it") == 0)
+        return new Item;
+    else if (type.find("col") == 0)
+        return new Collectibe;
+   else if (type.find("coi") == 0)
+        return new Coin;
+   else if (type.find("mo") == 0)
+        return new MovingPlatform;
+   else if (type.find("play") == 0)
+	   return new Player;
+   else if (type.find("plat") == 0)
+	   return new Platform;
+    else
+        return NULL;
 }
--- world.h
+++ world.h
@@ -1,10 +1,62 @@
+
 #ifndef WORLD_H
 #define WORLD_H
 
-class World
-{
+#include <string>
+#include <vector>
+
+#include "object.h"
+
+using namespace std;
+
+class World {
+    vector<Object*> objects;
+
+    // singleton instance
+    static World world;
+
+    // singleton: private ctors
+    World() { }
+    World(const World& that) = delete;  // don't allow copying    
+
 public:
-    World();
+
+
+    Object *createObject(const string& type);
+
+    // adds <obj> to objects in world
+    void add(Object *obj) {
+        auto it = objects.begin();
+        for (; it != objects.end(); ++it) {
+            if (obj->getId() < (*it)->getId()) {
+                objects.insert(it, obj);
+                break;
+            }
+        }
+        if (it == objects.end()) {
+            objects.push_back(obj);
+        }
+    }
+
+    // returns object with <id>, or NULL if none
+    Object *getById(int id);
+
+    vector<WorldObject*>& getObjects() { return objects; }
+
+    // Removes object with <id> from objects and returns it, or returns NULL if not found
+    Object *destroy(int id);
+
+    // Resets world
+    void reset();
+
+    // Destroy objects
+    ~World();
+
+    // Get singleton
+    static World& instance() { return world; }
 };
+
+
+extern World world;
 
 #endif // WORLD_H
