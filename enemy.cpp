329fff5d503f521e8f7bda8a4e4785a205101f25
Andrew <abprice96@yahoo.com>
2016-11-12 22:16:15 -0500

    Fixed a problem I created while trying to make the code faster

--- enemy.cpp
+++ enemy.cpp
@@ -95,14 +95,8 @@ void Enemy::move()
     }
 
     y += ySpeed;
-
-    Object* obj;
     for (size_t i = 0; i < World::instance().getObjects().size(); i ++)
     {
-        obj = World::instance().getObjects().at(i);
-        if (!QRect(obj->getX(),obj->getY(),obj->getWidth(),obj->getHeight()).intersects(World::instance().getCurrentScreen())) {
-            continue;
-        }
         CollisionDetails* col = checkCollision(World::instance().getObjects().at(i));
         if (col != NULL)
         {
@@ -198,13 +192,8 @@ void FlyingEnemy::move()
         right = !right;
         xCount = 0;
     }
-    Object* obj;
     for (size_t i = 0; i < World::instance().getObjects().size(); i ++)
     {
-        obj = World::instance().getObjects().at(i);
-        if (!QRect(obj->getX(),obj->getY(),obj->getWidth(),obj->getHeight()).intersects(World::instance().getCurrentScreen())) {
-            continue;
-        }
         CollisionDetails* col = checkCollision(World::instance().getObjects().at(i));
         if (col != NULL)
         {
